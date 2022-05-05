e1119bacc9832381b15aef0e192881f808a7ef5f
Sadican Üstün <sadican@hotmail.com.tr>
2022-05-05 18:03:23 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -35,7 +35,7 @@ echo "Mounting completed."
 # Start CTFd
 echo "Starting CTFd"
 exec gunicorn 'CTFd:create_app()' \
-    --bind '0.0.0.0:8000' \
+    --bind '0.0.0.0:8080' \
     --workers $WORKERS \
     --worker-tmp-dir "$WORKER_TEMP_DIR" \
     --worker-class "$WORKER_CLASS" \
